8953edcc301137f50b23c55abeab02d6ede159c5
+ <+>
2023-11-04 18:28:55 -0400

    just checking connection

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution in this file!
+function employees(key,value){
+const employee ={
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+return employee;
+}
+
+console.log(employees);
